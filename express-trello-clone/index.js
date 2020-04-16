2d3468e595f0938744c590873a15c498d87bab62
Nga Nguyen <ngasdragon@gmail.com>
2020-04-16 14:01:28 -0700

    Connected react with backend node

--- express-trello-clone/index.js
+++ express-trello-clone/index.js
@@ -1,7 +1,26 @@
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.listen(8080, () => {
-  console.log("Server is listening on port: 8080");
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use("/hello", (req,res) => {
+  res.send("hello world");
+})
+
+app.get("/express_backend", (req,res) => {
+  res.send({ express: 'backend is connected to react'})
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
 })
