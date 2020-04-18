74bee0e3e99c84b152ad28e0cbaf1db0818fd373
Nga Nguyen <ngasdragon@gmail.com>
2020-04-17 17:13:51 -0700

    Used mongodb atlas to manage database

--- express-trello-clone/models/index.js
+++ express-trello-clone/models/index.js
@@ -1,13 +1,15 @@
+const TRELLO_PASSWORD = process.env.TRELLO_PASSWORD;
 const mongoose = require('mongoose');
 
 const Users = require('./users.model');
-// Connection URL
-const url = 'mongodb://localhost:27017';
 
 // Database Name
 const dbName = 'trello-clone';
  
+// Connection URL
+const url = `mongodb+srv://trello:${TRELLO_PASSWORD}@cluster0-onasa.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
 // Use connect method to connect to the server
-mongoose.connect(url + "/" + dbName, { useNewUrlParser: true }, (err) => {
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   (!err) ? console.log("Connected successfully to database") : console.log('Error connecting to database');
 });
--- express-trello-clone/routes/users.js
+++ express-trello-clone/routes/users.js
@@ -2,11 +2,20 @@ const router = require("express").Router();
 const mongoose  = require('mongoose');
 
 const UserModel = mongoose.model("Users")
+
+// const user = {
+//   first_name: "Sherry",
+//   last_name: "Nguyen",
+//   email: "123@123.com",
+//   password: "123"
+// }
+// const newUser = new UserModel(user)
+// newUser.save()
 router.get("/login", (req,res) => {
   UserModel.find((err,docs) => {
     if (!err) {
       console.log(docs)
-      res.redner("users")
+      res.json(docs)
     } else {
       res.send(err)
     }
